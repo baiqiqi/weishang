e69d2b2ae8f7f0ad7162df72a49040b4e92c6ca2
zsm <1962611416@qq.com>
2016-05-09 09:20:13 +0800

    5/9

--- Public/Conf/config.php
+++ Public/Conf/config.php
@@ -1,10 +1,1 @@
-<?php
-	header('Content-Type:text/html; charset=utf-8');
-	error_reporting(E_ALL & ~E_NOTICE);		define('DB_HOST', 'localhost');	define('DB_USER', 'hfxxs');	define('DB_PWD', 'D7dFQBGEZfchfff6');	define('DB_NAME', 'hfxxs');	define('DB_PREFIX', 'wemall_');
-	
-	$conn = @mysql_connect(DB_HOST, DB_USER, DB_PWD) or die('数据库链接失败：'.mysql_error());
-	
-	@mysql_select_db(DB_NAME) or die('数据库错误：'.mysql_error());
-	
-	@mysql_query('SET NAMES UTF8') or die('字符集错误：'.mysql_error());
-?>
+<?php	header('Content-Type:text/html; charset=utf-8');	error_reporting(E_ALL & ~E_NOTICE);		define('DB_HOST', '123.57.66.116');	define('DB_USER', 'aqian');	define('DB_PWD', 'root');	define('DB_NAME', 'dajiangtang');	define('DB_PREFIX', 'wemall_');		$conn = @mysql_connect(DB_HOST, DB_USER, DB_PWD) or die('数据库链接失败：'.mysql_error());		@mysql_select_db(DB_NAME) or die('数据库错误：'.mysql_error());		@mysql_query('SET NAMES UTF8') or die('字符集错误：'.mysql_error());?>
